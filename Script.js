0c460abb44ba23346c0c23df5bb5ea8440921dd5
vat134 <arsenij3838@gmail.com>
2025-05-23 16:47:31 +0300

    3 vesion ar komentariem un labojumiem

--- Script.js
+++ Script.js
@@ -1,5 +1,12 @@
+//Kartes elementa izveide ar RTK koordinātēm
 const map = L.map('map').setView([56.964234, 24.164343], 13);
 
+//kartes pievienošana
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '© OpenStreetMap autori'
 }).addTo(map);
+
+//Norāde ar nosaukumu virs punkta
+L.marker([56.964234, 24.164343]).addTo(map)
+.bindPopup('RTK')
+.openPopup();
